6f612be17f6e99a6b37512638934e08f5e57353c
Laetitia Gcv <laetitia.gcv@gmail.com>
2020-05-11 10:40:36 +0200

    quest 3 done

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,21 +1,18 @@
 <?php
+namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
-namespace App\Controller;
-
-
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/wild", name="app_index")
+     * @Route("/", name="app_index")
      */
     public function index() :Response
     {
-        return $this->render('wild/home.html.twig', [
+        return $this->render('/default/home.html.twig', [
             'home' => 'Welcome',
         ]);
     }
