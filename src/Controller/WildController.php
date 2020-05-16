c6b0a67d45476e155ae4c646e7bca9c0e055a919
Laetitia Gcv <laetitia.gcv@gmail.com>
2020-05-16 17:12:40 +0200

    quest 5

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -7,17 +7,33 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
+/**
+ * Class WildController
+ * @package App\Controller
+ * @route("/wild", name="wild_")
+ */
 
 Class WildController extends AbstractController
 {
     /**
-     * @Route("/wild", name="wild_index")
+     * @Route("/", name="index")
      */
     public function index() :Response
     {
         return $this->render('wild/index.html.twig', [
-            'website' => 'Wild Séries',
+            'website' => 'Wild Series',
+        ]);
+    }
+    /**
+     * @Route("/show/{slug}", requirements={"slug"="[a-z0-9-]+"}, name="show")
+     * @param string $slug
+     * @return Response
+     */
+    public function show(string $slug = ''): Response
+    {
+        $slug = ucwords(str_replace('-',' ', $slug));
+        return $this->render('wild/show.html.twig', [
+            'slug' => $slug,
         ]);
     }
 
